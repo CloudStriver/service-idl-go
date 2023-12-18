8eb4715e8d3eec135c5fa785da435ee39a4a3601
Lansong <2095621390@qq.com>
2023-12-18 11:34:45 +0800

    feat: 提交usercenter.pb

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ if [ $? != 0 ];then
 fi
 for file in $files
 do
-  goctl rpc protoc $file --go_out=../ --go-grpc_out=../ --zrpc_out=../ --style=goZero
+  proto_path=$(dirname $files)
+  file_base=$(basename $files .proto)
+   goctl rpc protoc --proto_path=$proto_path $files --go_out=./go-zero-gen/$file_base --go-grpc_out=./go-zero-gen/$file_base --zrpc_out=./go-zero-gen/$file_base --style=goZero
+  rm -rf go-zero-gen/$file_base/etc
+  rm -rf go-zero-gen/$file_base/internal
+  rm -rf go-zero-gen/$file_base/$file_base.go
   if [ $? != 0 ];then
     exit 2
   fi
